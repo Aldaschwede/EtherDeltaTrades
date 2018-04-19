88cd0364229f4640e0bf7229c27e65e2f89e87be
Feni <stefan.j.eisele@gmail.com>
2018-04-19 18:45:13 +0200

    Addes ETH, because it was removed from Forkdelta Script

--- tokens.py
+++ tokens.py
@@ -18,7 +18,7 @@ def getTokens():
     edTokendata = edTokendata['tokens'] 
     
     allTokens={}
+    allTokens['0x0000000000000000000000000000000000000000'] = tokens('ETH', '0x0000000000000000000000000000000000000000', 18)
     for i in range(len(edTokendata)):
       allTokens[edTokendata[i]['addr']] = tokens(edTokendata[i]['name'], edTokendata[i]['addr'], edTokendata[i]['decimals'])
-    
     return allTokens
